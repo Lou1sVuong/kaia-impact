147fc976937417e7d1eb516e14d2048ebdfeb475
lou1s vuong <lou1svuong.dev@gmail.com>
2024-12-09 03:33:57 +0700

    Add Toaster, ThemeProvider, and WalletProviders components for enhanced UI and wallet integration
    - Introduced Toaster component for managing toast notifications.
    - Created ThemeProvider to handle theme context with Next Themes.
    - Implemented WalletProviders for wallet connection using RainbowKit and Wagmi.
    - Integrated QueryClient for React Query support and configured wallet options for Klaytn chain.

---
+++ providers/wallet-provider.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import * as React from 'react'
+import { RainbowKitProvider, getDefaultWallets, getDefaultConfig, darkTheme } from '@rainbow-me/rainbowkit'
+import { trustWallet, ledgerWallet } from '@rainbow-me/rainbowkit/wallets'
+import { baseSepolia, kairos, base } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { WagmiProvider, http } from 'wagmi'
+
+const { wallets } = getDefaultWallets()
+
+const config = getDefaultConfig({
+  appName: 'athena',
+  projectId: '455a9939d641d79b258424737e7f9205',
+  wallets: [
+    ...wallets,
+    {
+      groupName: 'Other',
+      wallets: [trustWallet, ledgerWallet]
+    }
+  ],
+  chains: [kairos],
+  transports: {
+    [kairos.id]: http('https://rpc.ankr.com/klaytn_testnet')
+  },
+  ssr: true
+})
+
+const queryClient = new QueryClient()
+
+export function WalletProviders({ children }: { children: any }) {
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          initialChain={kairos}
+          showRecentTransactions={true}
+          theme={darkTheme({
+            accentColor: '#4bdf74',
+            accentColorForeground: 'white',
+            borderRadius: 'none'
+          })}
+          locale='en-US'
+        >
+          {mounted && children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
